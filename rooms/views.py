9f2a22aa395539d131c3d9c8e096d8d7c0640a4c
Max16 <max16@naver.com>
2021-10-19 16:27:08 +0900

    Pagination by Paginator

--- rooms/views.py
+++ rooms/views.py
@@ -1,15 +1,14 @@
-from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.shortcuts import render, redirect
+from django.core.paginator import EmptyPage, Paginator
 from . import models
 
 
 def all_rooms(request):
-    page = request.GET.get("page")
+    page = request.GET.get("page", 1)
     room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10)
-    rooms = paginator.get_page(page)
-    print(vars(rooms.paginator))
-    return render(
-        request, "rooms/home.html", context={"rooms": rooms}
-    )  # html 파일 명은 /templates 안의 html 파일명과 일치해야됨
-    # context - html 로 넘기는 변수
+    paginator = Paginator(room_list, 10, orphans=5)
+    try:
+        rooms = paginator.page(page)
+        return render(request, "rooms/home.html", context={"page": rooms})
+    except EmptyPage:
+        return redirect("/")
