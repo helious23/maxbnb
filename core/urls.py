4b26ed7584ea0dd579c8d8a5478868ad675f1b12
Max16 <max16@naver.com>
2021-10-20 11:42:55 +0900

    URLs and Arguments

--- core/urls.py
+++ core/urls.py
@@ -5,3 +5,5 @@ app_name = "core"
 
 urlpatterns = [path("", room_views.HomeView.as_view(), name="home")]
 # room_views.함수명은 /rooms/views.py 의 함수명과 일치해야됨
+# class based view 사용 시, path("url", class경로.as_view(), name="xxx")
+# class 를 functoin 으로 변환 : as_view() 사용
--- rooms/views.py
+++ rooms/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView
+from django.shortcuts import render
 from . import models
 
 
@@ -9,12 +10,11 @@ class HomeView(ListView):
     model = models.Room
     paginate_by = 10
     paginate_orphans = 5
-    ordering = "created"
+    ordering = "host__superhost"
     page_kwarg = "page"
     context_object_name = "rooms"
 
-    def get_context_data(self, **kwargs):  # context 수정할 수 있는 함수
-        context = super().get_context_data(
-            **kwargs
-        )  # super() 로 기존의 context 를 유지시켜 줘야 됨
-        return context
+
+def room_detail(request, pk):
+    print(pk)
+    return render(request, "rooms/detail.html")
