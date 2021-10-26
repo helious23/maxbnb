b3aed116f1a044874ddde8c4fde3e2b176787dfe
Max16 <max16@naver.com>
2021-10-27 08:19:55 +0900

    user login & logout harder way

--- config/urls.py
+++ config/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("", include("core.urls", namespace="core")),
     path("rooms/", include("rooms.urls", namespace="rooms")),
+    path("users/", include("users.urls", namespace="uesrs")),
     path("admin/", admin.site.urls),
 ]
 
---
+++ users/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from . import models
+
+
+class LoginForm(forms.Form):
+
+    """Login Form Definition"""
+
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)  # password 입력시 사용
+
+    def clean(self):
+        # 두 fields 가 서로 상관이 있으므로(코드가 중복되므로) clean method 로 함께 관리함
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
+        try:
+            user = models.User.objects.get(email=email)
+            if user.check_password(password):
+                return self.cleaned_data
+                # cleaned_data 를 return 해야 email, password 둘 다 가진 data 가 return
+            else:
+                self.add_error("password", forms.ValidationError("Incorrect Password"))
+                # add_error("field", error) : 어디서 error 가 일어나는지 명확하게 보여줌
+                # clean_email() 이나 clean_password() 사용시에는 필요 없음
+        except models.User.DoesNotExist:
+            self.add_error("email", forms.ValidationError("User Not Found"))
