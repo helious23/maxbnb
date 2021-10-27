fb20c913610711ec81157abc95a0a35d1ad2fc41
Max16 <max16@naver.com>
2021-10-27 15:40:05 +0900

    sign up with formview

--- users/forms.py
+++ users/forms.py
@@ -24,3 +24,39 @@ class LoginForm(forms.Form):
                 # clean_email() 이나 clean_password() 사용시에는 필요 없음
         except models.User.DoesNotExist:
             self.add_error("email", forms.ValidationError("User Not Found"))
+
+
+class SignUpForm(forms.Form):
+
+    first_name = forms.CharField(max_length=80)
+    last_name = forms.CharField(max_length=80)
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    password1 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        try:
+            models.User.objects.get(email=email)
+            raise forms.ValidationError("Already Using Email")
+        except models.User.DoesNotExist:
+            return email
+
+    def claen_password1(self):
+        password = self.cleaned_data.get("password")
+        password1 = self.cleaned_data.get("password1")
+
+        if password != password1:
+            raise forms.ValidationError("Password does not match")
+        else:
+            return password
+
+    def save(self):
+        first_name = self.cleaned_data.get("first_name")
+        last_name = self.cleaned_data.get("last_name")
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
+        user = models.User.objects.create_user(email, email, password)
+        user.first_name = first_name
+        user.last_name = last_name
+        user.save()
